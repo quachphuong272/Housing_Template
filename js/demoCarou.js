5ea3890ac80323ffad8b96ca31fa932fc5d6012b
quachphuong272 <hannychan123@gmail.com>
2021-02-25 11:35:11 +0700

    'commit'

---
+++ js/demoCarou.js
@@ -0,0 +1,8 @@
+$(".slider-nav").slick({
+    autoplay: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,  
+    prevArrow: '<i class="fa fa-angle-left left"></i>',
+    nextArrow: '<i class="fa fa-angle-right right"></i>'
+  });
+  
---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    // direction: "vertical",
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination"
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".button-next",
+      prevEl: ".button-prev"
+    }
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar"
+    // }
+  });
+  
